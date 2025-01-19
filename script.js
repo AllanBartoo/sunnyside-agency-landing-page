7095d9badd81ac99284c00746ddfd48e1418a581
Allan Chementer <allanchementer@gmail.com>
2025-01-19 11:31:04 +0300

    did some adjustments

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const menubtn = document.getElementById('menu');
-const navlinks = document.getElementById('navlinks');
+document.addEventListener("DOMContentLoaded", () => {
+  const menubtn = document.getElementById("menu");
+  const navlinks = document.getElementById("navlinks");
+  const body = document.body;
 
-menubtn.addEventListener('click', () => {
-    navlinks.classList.toggle('show');
-})
+  menubtn.addEventListener("click", () => {
+    navlinks.classList.toggle("show");
+    body.classList.toggle("no-scroll");
+  });
+});
